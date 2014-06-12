b206ea51567901aadd724756cb62d3508b833978
Chris Eisenbraun <hello@chriseisenbraun.com>
2014-06-12 16:55:58 -0700

    Add Readme.md

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
- res.render('index', { title: 'Contact me', description: 'This is a new demo' });
+ res.render('index', { title: 'Contact Me', description: 'This is a new demo' });
 });
-
-
-  //res.render('index', { title: 'nvsSSS' });
-//});
 
 module.exports = router;
